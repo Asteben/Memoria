6403027e5f79b0c674bf1ac5f492cff884551726
Asteben <esteban.leon@mail.udp.cl>
2022-04-19 23:46:18 -0400

    Corpusgen en Python y cambios al Limpiador de dataset

--- Predictor/Cleaners/limpia3.py
+++ Predictor/Cleaners/limpia3.py
@@ -1,3 +1,7 @@
-import pandas as pd
-df = pd.read_json (r'dataset/dataset.jsonl')
-df.to_csv (r'dataset/datasetFinal.csv', index = None)
+import time, datetime
+dt = "Thu Mar 19 19:52:18 +0000 2020"
+dts = dt.split()
+dtf = dts[:4]
+dtf.append(dts[5])
+dt = ' '.join(dtf)
+print ((datetime.datetime.strptime(dt, "%a %b %d %H:%M:%S %Y")).timestamp())
