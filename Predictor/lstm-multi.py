cc401e03b5e2c1e4b32ce838c7accd4a15928d64
Asteben <esteban.leon@mail.udp.cl>
2022-05-30 03:10:22 -0400

    commit

--- Predictor/lstm-multi.py
+++ Predictor/lstm-multi.py
@@ -6,10 +6,10 @@ import numpy as np
 from keras.preprocessing.sequence import TimeseriesGenerator
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras import (Model, Input)
+from keras.layers import (LSTM, Dense, Activation, BatchNormalization, Dropout, Bidirectional, Add)
 
 print (tf.__version__)
-
 
 def load_data ( inputPath ):
     cols = ["Unix", "Quantity"]
@@ -18,7 +18,7 @@ def load_data ( inputPath ):
     return df1
 
 
-dataset_path = "../Data/dataset/dataset_groupby_60s.csv"
+dataset_path = "../FinalSystem/Data/corona_full-GBy600s.csv"
 
 raw_dataset = load_data( dataset_path )
 dataset = raw_dataset.copy()
@@ -28,8 +28,8 @@ dataset = dataset.dropna()
 
 len_data = len(dataset)
 sample_rate = 0.8
-look_back  = 250
-k = 60
+look_back  = 200
+k = 200
 
 df_train = dataset.iloc[:int(len_data*sample_rate)]
 Test = dataset.iloc[int(len_data*sample_rate):]
@@ -62,12 +62,27 @@ model.add(LSTM(n_neurons, input_shape=(look_back, 1)))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
 '''
+
 model = Sequential()
-model.add(LSTM(32, input_shape=(look_back, 1) ,return_sequences=True))
-model.add(Dense(16, activation='relu'))
+model.add(LSTM(500, input_shape=(look_back, 1),  kernel_regularizer='l2',return_sequences=True ))
+model.add(LSTM(250,kernel_regularizer='l2'))
+model.add(Dense(100, activation='relu'))
 model.add(Dense(k))
+
+'''
+inputs = Input(shape=(look_back, 1))
+
+bd_seq = Bidirectional(LSTM(128, return_sequences=True, kernel_regularizer='l2'), merge_mode='sum')(inputs)
+bd_sin = Bidirectional(LSTM(32, return_sequences=True, kernel_regularizer='l2'), merge_mode='sum') (bd_seq)
+
+bd_1 = Bidirectional(LSTM(1), merge_mode='sum')(bd_seq)
+bd_2 = Bidirectional(LSTM(1), merge_mode='sum')(bd_sin)
+output = Add()([bd_1, bd_2])
+
+model = Model(inputs=inputs, outputs=output)'''
 model.compile(loss='mse', optimizer='adam')
-model.fit(final,epochs=150, verbose=2)
+model.fit(final,epochs=15, verbose=2)
+model.save(f'Multistep-Lstm-GBy600s-LB{look_back}-{k}K')
 '''
 #model = tf.keras.models.load_model('../FinalSystem/Models/Lstm-LB250-32N-Corona-Gby60')
 test_predictions = []
@@ -84,7 +99,7 @@ for i in range(len(Test.index)):
 '''
 first_eval_batch = np.array(Y_train.tail(look_back).tolist())
 current_batch = np.reshape(first_eval_batch,(1, look_back, 1))
-test_predictions = model.predict(current_batch)
+test_predictions = model.predict(current_batch)[0]
 
 fig, ax = plt.subplots(1, 1, figsize=(15, 5))
 ax.plot(X_train,Y_train, lw=2, label='train data')
