77482b040b1e3475c9a4ee0fbba4df3a797f90dc
Asteben <esteban.leon@mail.udp.cl>
2022-07-10 22:46:36 -0400

    commit

--- Arista1/Predictor/Cleaners/Test.py
+++
@@ -1,7 +0,0 @@
-import time, datetime
-dt = "Thu Mar 19 19:52:18 +0000 2020"
-dts = dt.split()
-dtf = dts[:4]
-dtf.append(dts[5])
-dt = ' '.join(dtf)
-print ((datetime.datetime.strptime(dt, "%a %b %d %H:%M:%S %Y")).timestamp())
